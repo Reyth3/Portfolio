953c4d69722c6e7d7e02b03dd6c261af7e90abcb
Reyth3 <mrreynevan2@gmail.com>
2016-12-13 09:29:58 +0100

    Added website

---
+++ MyPortfolio/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using MyPortfolio.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPortfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SetLang(int id)
+        {
+            LanguageSwitch.SetLang((Lang)id);
+            return RedirectToAction(Request.UrlReferrer.AbsoluteUri);
+        }
+    }
+}
---
+++ MyPortfolio/Helpers/LanguageSwitch.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyPortfolio.Helpers
+{
+    public class LanguageSwitch
+    {
+        public static Lang GetLanguage()
+        {
+            var c = HttpContext.Current.Request.Cookies.Get("lang");
+            if (c == null)
+                return (Lang)0;
+            else return (Lang)int.Parse(c["language"]);
+        }
+
+        public static void SetLang(Lang l)
+        {
+            var lang = new HttpCookie("lang");
+            lang.Expires = DateTime.Now.AddDays(7);
+            lang["language"] = ((int)l).ToString();
+            HttpContext.Current.Response.Cookies.Set(lang);
+        }
+    }
+
+    public enum Lang { English, Polish }
+}
